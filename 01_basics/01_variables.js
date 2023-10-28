21037125cdae062a238d9cf2f15fd22c810e9371
deepanshu127001 <deepanshu127001@gmail.com>
2023-10-28 23:28:48 +0530

    conversion

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId=3422334342;
+let emailId="deepanshu127001@gmail.com";
+var accountPassword="12700001";
+// accountId=45; error can't change the const 
+temp=78
+console.log(accountId);
+console.log(typeof temp);
+console.table([accountId,emailId,accountPassword])
+
+
+
+
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,10 @@
+let score = "33a"
+console.log(Number(score));
+//33 Number
+//33a NaN
+let num=1
+
+console.log(Boolean(num));
+console.log(Boolean(""));
+console.log(Boolean("str"));
+console.log(String(num));
